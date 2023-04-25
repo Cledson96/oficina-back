d0963a962ef54c2ccc81b4eee691476d28ea184d
CLEDSON SANTOS SOUZA <cledson1996@gmail.com>
2023-04-25 11:33:57 -0300

    Delete dist directory

--- dist/routes/signIn.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const signIn_controller_1 = require("../controllers/signIn.controller");
-const router = (0, express_1.Router)();
-router.post("/sign_in", signIn_controller_1.signIn);
-exports.default = router;
-//# sourceMappingURL=signIn.js.map
--- dist/routes/signUp.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const signUp_controller_1 = require("../controllers/signUp.controller");
-const router = (0, express_1.Router)();
-router.post("/sign_up", signUp_controller_1.signUp);
-exports.default = router;
-//# sourceMappingURL=signUp.js.map
