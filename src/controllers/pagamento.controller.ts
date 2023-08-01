e5f9f7282e72ef29d0d796d393c3b4f43a8a9520
cledson <cledson1996@gmail.com>
2023-08-01 01:20:01 -0300

    teste

--- src/controllers/pagamento.controller.ts
+++ src/controllers/pagamento.controller.ts
@@ -10,7 +10,40 @@ export function pagamento(req: Request, res: Response) {
   console.log(req.body)
   console.log(preference);
   mercadopago.preferences
-    .create(preference)
+    .create({
+      items: [
+        {
+          id: "item-ID-1234",
+          title: "Meu produto",
+          currency_id: "BRL",
+          picture_url: "https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
+          description: "Descrição do Item",
+          category_id: "art",
+          quantity: 1,
+          unit_price: 75.76
+        },
+      ],
+      payer: {
+        name: "João",
+        surname: "Silva",
+        email: "user@email.com",
+        phone: {
+          area_code: "11",
+          number: "4444-4444"
+        },
+
+        address: {
+          street_name: "Street",
+          street_number: 123,
+          zip_code: "06233200"
+        }
+      },
+      back_urls: {
+        success: "http://localhost:3000/concluido",
+        failure: "http://localhost:3000/concluido",
+      },
+      auto_return: "approved",
+    })
     .then((response) => {
       res.status(200).send(response.body.id);
       return;
